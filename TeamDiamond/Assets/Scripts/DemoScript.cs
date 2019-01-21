c51424f47f1f398d9467a59cd278290602996391
Marcus Anderson <andma305@student.otago.ac.nz>
2019-01-21 15:47:10 +1300

    Added in fading effect between scenes

---
+++ TeamDiamond/Assets/Scripts/DemoScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class DemoScript : MonoBehaviour {
+    //name of the scene you want to load
+    public string scene;
+	public Color loadToColor = Color.white;
+
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            GoFade();
+        }
+    }
+
+    public void GoFade()
+    {
+        Initiate.Fade(scene, loadToColor, 1.0f);
+    }
+}
