a7de13f755aca3e880e95ad706e5551d4f1ca5b1
Grace Auckram <aucgr972@student.otago.ac.nz>
2019-01-21 17:53:33 +1300

    bird now walks free

--- TeamDiamond/Assets/Scripts/PlayerMove.cs
+++ TeamDiamond/Assets/Scripts/PlayerMove.cs
@@ -1,36 +1,79 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+    Created by: Lech Szymanski
+                lechszym@cs.otago.ac.nz
+                Jan 19, 2016            
+*/
+
 using UnityEngine;
+using System.Collections;
 
-public class PlayerMove : MonoBehaviour
-{
+public class PlayerMove : MonoBehaviour {
 
-    public LayerMask whatIsGround;
-    public Transform groundCheck;
-    public bool isGrounded;
-    public float jumpForce;
-    public float speed;
-    Rigidbody2D rb;
+    public float speed = 2;
+    public float jumpPower = 5;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    private Rigidbody2D body = null;
+    private bool canJump = false;
+    public bool facingRight = true;
+    public bool isChild = false;
+
+    // Use this for initialization
+    void Start () {
+        //Get the reference to rigid body comonent
+        body = transform.GetComponent<Rigidbody2D> ();
     }
 
-    void Update()
-    {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+    void OnCollisionEnter2D(Collision2D collision) {
+        //If not in collisions with something, and not moving up and down then not airborne
+        float verticalSpeed = Mathf.Abs(body.velocity.y);
+        if(verticalSpeed < 0.001) {
+            canJump = true;
+        }   
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        //If not in collision with something, then airborne
+        canJump = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        //Get movement input
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis ("Vertical");
+
+        //Only going up is allowed when jumping and
+        //only when not airborne
+        if (canJump && v > 0) {
+            v = jumpPower;
+        } else {
+            v = 0;
+        }
+
+        if (body != null) {
+            //Add movement forces to the body
+            if (!isChild)
+            {
+                body.AddForce(new Vector2(0, v), ForceMode2D.Impulse);
+            }
+            body.velocity = new Vector2(h * speed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (h > 0 && !facingRight)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
+            Flip();
+        }
+        else if (h < 0 && facingRight)
+        {
+            Flip();
         }
     }
 
-    void FixedUpdate()
+    void Flip()
     {
-        isGrounded = Physics2D.OverlapPoint(groundCheck.position, whatIsGround);
-        float x = Input.GetAxis("Horizontal");
-        Vector3 move = new Vector3(x * speed, rb.velocity.y, 0f);
-        rb.velocity = move;
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
