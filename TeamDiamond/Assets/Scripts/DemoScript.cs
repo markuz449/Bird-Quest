7531ec5480c88b307e02072753664f6bfd5a2c30
Marcus Anderson <andma305@student.otago.ac.nz>
2019-01-22 14:46:19 +1300

    Added tile changing on spacebar press

--- TeamDiamond/Assets/Scripts/DemoScript.cs
+++ TeamDiamond/Assets/Scripts/DemoScript.cs
@@ -5,12 +5,38 @@ public class DemoScript : MonoBehaviour {
     //name of the scene you want to load
     public string scene;
 	public Color loadToColor = Color.white;
+    public GameObject background;
+    public GameObject backgroundNight;
+    public GameObject platforms;
+    public GameObject platformsNight;
+    public bool state = true;
+
+    /*private void Start()
+    {
+        background.SetActive(true);
+        backgroundNight.SetActive(false);
+        platforms.SetActive(true);
+        platformsNight.SetActive(false);
+    }*/
 
     void Update()
     {
-        if (Input.GetButton("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
-            GoFade();
+            if (state == true)
+            {
+                background.SetActive(false);
+                backgroundNight.SetActive(true);
+                platforms.SetActive(false);
+                platformsNight.SetActive(true);
+                state = false;
+            } else{
+                background.SetActive(true);
+                backgroundNight.SetActive(false);
+                platforms.SetActive(true);
+                platformsNight.SetActive(false);
+                state = true;
+            }
         }
     }
 
