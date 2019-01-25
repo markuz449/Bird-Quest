db8b5a7f75daa1a7caf04030824683c98a2af4a5
Grace Auckram <aucgr972@student.otago.ac.nz>
2019-01-25 16:12:29 +1300

    fixed nathans mistakes. bouncyness removed and chick function restored

--- TeamDiamond/Assets/Scripts/Box.cs
+++ TeamDiamond/Assets/Scripts/Box.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(FixedJoint2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+
 public class Box : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        GetComponent<SpriteRenderer>().sortingLayerName = "Items";
+
+        GetComponent<Rigidbody2D>().mass = 5;
+        GetComponent<Rigidbody2D>().gravityScale = 3;
+
+        GetComponent<FixedJoint2D>().enabled = false;
+        transform.tag = "Box";
+        gameObject.layer = 8;
 	}
 	
 	// Update is called once per frame
