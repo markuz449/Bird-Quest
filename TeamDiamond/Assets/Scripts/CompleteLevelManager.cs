948cc937350db65f3ffc176cb8912f68fd292118
Jaydin McMullan <mcmja334@student.otago.ac.nz>
2019-02-03 16:53:05 +1300

    Now counts the number of resets and displays it when level complete.

--- TeamDiamond/Assets/Scripts/CompleteLevelManager.cs
+++ TeamDiamond/Assets/Scripts/CompleteLevelManager.cs
@@ -1,20 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CompleteLevelManager : MonoBehaviour {
+
+    public Text numResets = null;
+    private LevelMaster game;
+
+    public GameObject endGameResets;
+
+
+
+
+
+
 
     public GameObject completelevel;
 
 
     // Use this for initialization
     void Start () {
+
+
         completelevel.SetActive(false);
+        game = GameObject.FindGameObjectWithTag("LM").GetComponent<LevelMaster>();
+
+        endGameResets = GameObject.FindGameObjectWithTag("Resets");
+
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
+        //endGameResets.AddComponent. = "Total Resets: " + game.resets;
+
+
 		
 	}
 
