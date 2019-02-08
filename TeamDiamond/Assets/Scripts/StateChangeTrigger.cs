3493cd2f9f1dc2ece9a7ec9c2425316ac3354bda
Jack Baucke <bauja773@student.otago.ac.nz>
2019-02-08 14:29:43 +1300

    ooga booga

--- TeamDiamond/Assets/Scripts/StateChangeTrigger.cs
+++ TeamDiamond/Assets/Scripts/StateChangeTrigger.cs
@@ -8,11 +8,16 @@ public class StateChangeTrigger : MonoBehaviour {
     public GameObject day;
     public DayNight stateControl;
     Flower[] flower;
+    public GameObject[] flowerGrowShrinks = GameObject.FindGameObjectsWithTag("Items");
     public bool daytime;
 
     public void OnTriggerEnter2D(Collider2D collision){
 
         //if (daytime != stateControl.dayTime){
+
+        foreach (GameObject flowergrow in flowerGrowShrinks) {
+            flowergrow.transform.position += new Vector3(0, 2, 0);
+        }
             stateControl.StateChange(daytime);
             //if (daytime == true){
             //    foreach (Flower bloom in flower){
