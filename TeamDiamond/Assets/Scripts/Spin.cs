f387d116988e698fcd3c01e6cdce950a63af52c6
Grace Auckram <aucgr972@student.otago.ac.nz>
2019-02-11 10:38:40 +1300

    made the day / night changers spin and fixed level 1 - switching between day and night lag

---
+++ TeamDiamond/Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 150f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
