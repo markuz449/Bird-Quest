ecf0d0812830987f7cf6e9c2c38224dccaaea2d5
Jack Baucke <bauja773@student.otago.ac.nz>
2019-02-10 22:40:30 +1300

    The flowers sorta push up and down now...

---
+++ TeamDiamond/Assets/Scripts/ChangeToNight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToNight : MonoBehaviour
+{
+
+    Vector3 startPosition;
+    Vector3 location;
+    Vector3 destination;
+
+    //StateChangeTrigger stopper = new StateChangeTrigger();
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        startPosition = transform.position;
+        location = startPosition;
+        destination = new Vector3(transform.position.x, transform.position.y - 2, transform.position.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        location = Vector3.Lerp(location, destination, Time.deltaTime * 5);
+        transform.position = location;
+
+        if (location == destination)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
--- TeamDiamond/Assets/Scripts/StateChangeTrigger.cs
+++ TeamDiamond/Assets/Scripts/StateChangeTrigger.cs
@@ -11,25 +11,59 @@ public class StateChangeTrigger : MonoBehaviour {
     public GameObject[] flowerGrowShrinks;
     public bool daytime;
 
-    public void OnTriggerEnter2D(Collider2D collision){
+    public Vector3 currentPosition;
+    public Vector3 destination;
 
-        foreach (GameObject flowergrow in flowerGrowShrinks){
-            flowergrow.transform.position += new Vector3(0, 2, 0);
+    public static bool quit;
+
+    public float wait;
+
+
+
+
+   
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        foreach (GameObject flowergrow in flowerGrowShrinks)
+        {
+            if (daytime)
+            {
+
+ 
+                flowergrow.GetComponent<ChangeToNight>().enabled = false;
+                flowergrow.GetComponent<ChangeToDay>().enabled = true;
+                Debug.Log("test");
+                //flowergrow.GetComponent<ChangeToDay>().enabled = false;
+
+            }
+            else
+            {
+
+                flowergrow.GetComponent<ChangeToDay>().enabled = false;
+                flowergrow.GetComponent<ChangeToNight>().enabled = true;
+                Debug.Log("test");
+                //flowergrow.GetComponent<ChangeToNight>().enabled = false;
+            }
         }
-        
+
         stateControl.StateChange(daytime);
-        if (daytime == true){
-            foreach (Flower bloom in flower){
-                bloom.Bloom();
-            }
-        } else{
-            foreach (Flower bloom in flower){
-                bloom.Debloom();
-            }
-        }
-        // maybe don't destroy them??
-        //Destroy(gameObject);
+
+
+
+        //if (daytime == true){
+        //    foreach (Flower bloom in flower){
+        //        bloom.Bloom();
+        //    }
+        //} else{
+        //    foreach (Flower bloom in flower){
+        //        bloom.Debloom();
+        //    }
+        //}
+
     }
+
 
     private void Awake(){
         flower = day.GetComponentsInChildren<Flower>();
