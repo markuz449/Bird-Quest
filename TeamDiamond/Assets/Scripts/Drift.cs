a776588222c70f10e7507a34516e8b5b94f316eb
Grace Auckram <aucgr972@student.otago.ac.nz>
2019-02-11 15:50:28 +1300

    clouds now drift across the screen

---
+++ TeamDiamond/Assets/Scripts/Drift.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drift : MonoBehaviour {
+
+    public float speed = 0.003f;
+    Transform[] clouds;
+
+
+	// Use this for initialization
+	void Awake () {
+        int childern = transform.childCount;
+        clouds = new Transform[childern];
+        for (int i = 0; i < childern; i++){
+            clouds[i] = transform.GetChild(i);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position;
+        for (int i = 0; i < clouds.Length; i++){
+            position = clouds[i].localPosition;
+            position.x -= speed;
+            clouds[i].localPosition = position;
+            if(clouds[i].localPosition.x < -80f){
+                position.x = 80f;
+                clouds[i].localPosition = position;
+            }
+        }
+	}
+}
